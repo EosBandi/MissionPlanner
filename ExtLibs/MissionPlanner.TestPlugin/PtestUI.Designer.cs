83b82dc33e85805e820f5ab3662edac61daa73b4
Andras Schaffer <eosbandi@gmail.com>
2018-10-04 10:26:09 +0200

    Moved plugin development to a different branch

---
+++ ExtLibs/MissionPlanner.TestPlugin/PtestUI.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace MissionPlanner.TestPlugin
+{
+    partial class PtestUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ucPlayerControl1 = new Player.ucPlayerControl();
+            this.SuspendLayout();
+            // 
+            // ucPlayerControl1
+            // 
+            this.ucPlayerControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ucPlayerControl1.AutoRecconect = false;
+            this.ucPlayerControl1.ffmegParams = "";
+            this.ucPlayerControl1.ffmegPath = "";
+            this.ucPlayerControl1.Location = new System.Drawing.Point(0, 0);
+            this.ucPlayerControl1.MediaUrl = "";
+            this.ucPlayerControl1.Name = "ucPlayerControl1";
+            this.ucPlayerControl1.RecordPath = "";
+            this.ucPlayerControl1.Size = new System.Drawing.Size(545, 340);
+            this.ucPlayerControl1.TabIndex = 0;
+            this.ucPlayerControl1.VideoRate = Player.ucPlayerControl.ratelist.OriginalRate;
+            this.ucPlayerControl1.VisiblePlayerMenu = true;
+            this.ucPlayerControl1.VisibleStatus = true;
+            // 
+            // PtestUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 374);
+            this.Controls.Add(this.ucPlayerControl1);
+            this.Name = "PtestUI";
+            this.Text = "PtestUI";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Player.ucPlayerControl ucPlayerControl1;
+    }
+}
