c0f6ba8fbe7c8afd1ce5691fc07a7e4bf44fe441
Michael Oborne <mich146@hotmail.com>
2021-03-22 10:49:31 +1100

    Geotiff: change cache to work on android

--- ExtLibs/Utilities/GeoTiff.cs
+++ ExtLibs/Utilities/GeoTiff.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using GMap.NET;
 using log4net;
-using System.Runtime.Caching;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace MissionPlanner.Utilities
 {
