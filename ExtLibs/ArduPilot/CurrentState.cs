d49e1561d3ef40b3ce81fb8c7b10743222583739
Michael Oborne <mich146@hotmail.com>
2021-03-26 11:40:24 +1100

    CurrentState: fix disarmed status bug

--- ExtLibs/ArduPilot/CurrentState.cs
+++ ExtLibs/ArduPilot/CurrentState.cs
@@ -2297,8 +2297,9 @@ namespace MissionPlanner
                                 if(armed == false && newarmed == true)
                                 {
                                     timeSinceArmInAir = 0;
-                                    armed = newarmed;
                                 }
+
+                                armed = newarmed;
 
                                 // saftey switch
                                 if (armed && sensors_enabled.motor_control == false && sensors_enabled.seen)
