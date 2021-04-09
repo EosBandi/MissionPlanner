a606d75e92ae10c568ee7813921ad3ac65b673ac
Andras Schaffer <eosbandi@gmail.com>
2021-04-09 22:46:14 +0200

    Some WindDir changes, not finished

--- ExtLibs/Controls/WindDir.cs
+++ ExtLibs/Controls/WindDir.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace MissionPlanner.Controls
 {
@@ -58,13 +59,13 @@ namespace MissionPlanner.Controls
 
             base.OnPaint(e);
 
-            Rectangle outside = new Rectangle(1,1,this.Width - 3, this.Height -3);
+            //Rectangle outside = new Rectangle(1,1,this.Width - 3, this.Height -3);
 
-            e.Graphics.DrawArc(blackpen, outside, 0, 360);
+            //e.Graphics.DrawArc(blackpen, outside, 0, 360);
 
-            Rectangle inside = new Rectangle(this.Width / 4,this.Height / 4, (this.Width/4) * 2,(this.Height / 4) * 2);
+            //Rectangle inside = new Rectangle(this.Width / 4,this.Height / 4, (this.Width/4) * 2,(this.Height / 4) * 2);
 
-            e.Graphics.DrawArc(blackpen, inside, 0, 360);
+            //e.Graphics.DrawArc(blackpen, inside, 0, 360);
 
             double x = (this.Width / 2) * Math.Cos((_direction - 90) * deg2rad);
 
@@ -73,6 +74,10 @@ namespace MissionPlanner.Controls
             // full scale is 10ms
 
             double scale = Math.Max(maxspeed, Speed);
+
+            e.Graphics.DrawString(Speed.ToString("0"), this.Font, Brushes.Red, (float)5, (float)5);
+
+
 
             x = x / scale * Speed;
             y = y / scale * Speed;
@@ -94,16 +99,15 @@ namespace MissionPlanner.Controls
 
                 x1 = (this.Width / 7) * (float)Math.Cos((_direction + 60 + 180) * deg2rad);
                 y1 = (this.Height / 7) * (float)Math.Sin((_direction + 60 + 180) * deg2rad);
-                
+
                 e.Graphics.DrawLine(redpen, outx, outy, outx - x1, outy - y1);
             }
 
-            e.Graphics.DrawString(Speed.ToString("0"), this.Font, Brushes.Red, (float)0, (float)0);
         }
 
         protected override void OnPaintBackground(PaintEventArgs e)
         {
-            
+
             base.OnPaintBackground(e);
             //e.Graphics.Clear(Color.Transparent);
         }
@@ -112,5 +116,57 @@ namespace MissionPlanner.Controls
         {
             base.WndProc(ref m);
         }
+
+
+        // Rounded corners
+        private int radius = 10;
+        [DefaultValue(20)]
+        public int Radius
+        {
+            get { return radius; }
+            set
+            {
+                radius = value;
+                this.RecreateRegion();
+            }
+        }
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        private static extern IntPtr CreateRoundRectRgn(int nLeftRect, int nTopRect,
+            int nRightRect, int nBottomRect, int nWidthEllipse, int nHeightEllipse);
+
+        private GraphicsPath GetRoundRectagle(Rectangle bounds, int radius)
+        {
+            float r = radius;
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(bounds.Left, bounds.Top, r, r, 180, 90);
+            path.AddArc(bounds.Right - r, bounds.Top, r, r, 270, 90);
+            path.AddArc(bounds.Right - r, bounds.Bottom - r, r, r, 0, 90);
+            path.AddArc(bounds.Left, bounds.Bottom - r, r, r, 90, 90);
+            path.CloseFigure();
+            return path;
+        }
+
+        private void RecreateRegion()
+        {
+            var bounds = ClientRectangle;
+
+            this.Region = Region.FromHrgn(CreateRoundRectRgn(bounds.Left, bounds.Top,
+                bounds.Right, bounds.Bottom, Radius, radius));
+            this.Invalidate();
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+
+            this.RecreateRegion();
+        }
+        // End rounded rectangle
+
+
+
+
     }
 }
