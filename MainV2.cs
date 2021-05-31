424e29053f72c878f9c4c18224bf049d4d2c87a2
Michael Oborne <mich146@hotmail.com>
2021-05-31 22:30:59 +1000

    MAVState: cleanup params

--- MainV2.cs
+++ MainV2.cs
@@ -1734,9 +1734,10 @@ namespace MissionPlanner
                 if (getparams)
                 {
                     if (File.Exists(comPort.MAV.ParamCachePath) && 
-                        new FileInfo(comPort.MAV.ParamCachePath).LastWriteTime > DateTime.Now.AddDays(-1))
+                        new FileInfo(comPort.MAV.ParamCachePath).LastWriteTime > DateTime.Now.AddHours(-1))
                     {
-                        comPort.MAV.param = File.ReadAllText(comPort.MAV.ParamCachePath).FromJSON<MAVLink.MAVLinkParamList>();
+                        File.ReadAllText(comPort.MAV.ParamCachePath).FromJSON<MAVLink.MAVLinkParamList>()
+                            .ForEach(a => comPort.MAV.param.Add(a));
                     }
                     else
                     {
