2574ad6d5cbf9f2294241c55e0cb2886cbd741b9
Andras Schaffer <eosbandi@gmail.com>
2025-05-13 22:03:39 +0200

    Version 135

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,4 +29,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.3.*")]
-[assembly: AssemblyFileVersion("1.3.82.134")]
+[assembly: AssemblyFileVersion("1.3.82.135")]
