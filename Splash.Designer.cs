4dd7cdceae5a2d384e114b3ee7dbe40e11909a91
Andras Schaffer <eosbandi@gmail.com>
2025-09-10 13:51:48 +0200

    Release 140 Add dev name to splash screen

--- Splash.Designer.cs
+++ Splash.Designer.cs
@@ -41,13 +41,13 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.OliveDrab;
-            this.label1.Location = new System.Drawing.Point(447, 132);
+            this.label1.Location = new System.Drawing.Point(474, 132);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.Size = new System.Drawing.Size(98, 39);
             this.label1.TabIndex = 0;
             this.label1.Tag = "custom";
-            this.label1.Text = "by Michael Oborne";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label1.Text = "Michael Oborne\r\nBob Long\r\nAndras Schaffer\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // TXT_version
             // 
@@ -72,7 +72,6 @@
             // 
             // Splash
             // 
-
             this.BackgroundImage = global::MissionPlanner.Properties.Resources.splashdark;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(584, 336);
